c76aa66c6eee249fea9050bbaf24ccbc40cbdd6e
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-12-08 22:09:17 +0100

    create new vite project and start to make a terminal portfolio

---
+++ README.md
@@ -0,0 +1,2 @@
+# React + Vite
+a terminal attempt with react vite and a switch
---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from "react"
+
+
+function App() {
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("")
+  const inputRef =useRef()
+  
+  useEffect(() => {
+    inputRef.current.focus()
+  },[])
+
+  return (
+    <>
+     <div className="app" onClick={() => {inputRef.current.focus()}}>
+      <input 
+      type="text" 
+      ref={inputRef}
+      placeholder="ls"
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+      onKeyDown={(e) =>{
+        if (e.key === "Enter") {
+          let newOutput = '';
+          newOutput = output + '\n' + '$ ' + input + '\n '
+          switch (input) {
+            case "ls":
+              newOutput += "List of Projects";
+              break
+            case "pwd":
+              newOutput += "Nice! Your Welcome here!";
+              break;
+            default:
+              newOutput += "Unknow command- use ls"
+          }
+          setOutput(newOutput)
+          setInput('')
+        }
+      }}
+      />
+
+      <div className="terminal" >
+         {output}
+      </div>
+     </div>
+     
+    </>
+  )
+}
+
+export default App
