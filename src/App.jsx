9ab501e60f3923ba86b70264c19886d056f8d609
Diego-monfort-landot <diegomonfort1989@gmail.com>
2023-12-08 22:34:11 +0100

    added styles and clear

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,53 @@ import { useEffect, useRef, useState } from "react"
 function App() {
   const [input, setInput] = useState("")
   const [output, setOutput] = useState("")
-  const inputRef =useRef()
-  
+  const inputRef = useRef()
+
   useEffect(() => {
     inputRef.current.focus()
-  },[])
+  }, [])
 
   return (
     <>
-     <div className="app" onClick={() => {inputRef.current.focus()}}>
-      <input 
-      type="text" 
-      ref={inputRef}
-      placeholder="ls"
-      value={input}
-      onChange={(e) => setInput(e.target.value)}
-      onKeyDown={(e) =>{
-        if (e.key === "Enter") {
-          let newOutput = '';
-          newOutput = output + '\n' + '$ ' + input + '\n '
-          switch (input) {
-            case "ls":
-              newOutput += "List of Projects";
-              break
-            case "pwd":
-              newOutput += "Nice! Your Welcome here!";
-              break;
-            default:
-              newOutput += "Unknow command- use ls"
-          }
-          setOutput(newOutput)
-          setInput('')
-        }
-      }}
-      />
-      <div className="terminal" >
-         {output}
+      <div className="container-title">
+        <h1>Terminal</h1>
+        <p>pwd, ls and clear... more is comming...</p>
       </div>
-     </div>
-     
+
+      <div className="app" onClick={() => { inputRef.current.focus() }}>
+        <input
+          type="text"
+          ref={inputRef}
+          placeholder="ls"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              let newOutput = '';
+              newOutput = output + '\n' + '$ ' + input + '\n '
+              switch (input) {
+                case "ls":
+                  newOutput += "List of Projects";
+                  break
+                case "pwd":
+                  newOutput += "Nice! Your Welcome here!";
+                  break;
+                case "clear":
+                  location.reload();
+                  break
+                default:
+                  newOutput += "Unknow command- use ls"
+              }
+              setOutput(newOutput)
+              setInput('')
+            }
+          }}
+        />
+        <div className="terminal" >
+          {output}
+        </div>
+      </div>
+
     </>
   )
 }
